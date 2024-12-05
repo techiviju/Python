efa4908bfdb4486166a049d9ade1eeff146d15af
techiviju <technicalvijay5220@gmail.com>
2024-12-05 17:13:36 +0530

    New file add

---
+++ Python_Black_Book/04_ch_Operaro.py/03_math_fun.py
@@ -0,0 +1,33 @@
+import math as m
+
+x=4.5
+print("ceil :-",m.ceil(x))
+
+print("floor :-",m.floor(x))
+
+print("Exponentiation :-",m.exp(x))
+
+print("\nfabs Positive Quentity :-",m.fabs(-4.56))
+
+print("Factorial 4 :-",m.factorial(4))
+
+print("Returns Reminders of Division fmod(14.5,3) :-",m.fmod(14.5,3))
+
+print("\nReturn accurate sum of floting points fsum([x,1.5,2.4,-3.2] :-",m.fsum([x,1.5,2.4,-3.2]))
+
+print("modf(x) Return float and Integral part :-",m.modf(x))
+
+print("\nsqrt(49) Return positive square root :-",m.sqrt(49))
+
+print("pow(5,3) Power :-",m.pow(5,3))
+
+
+    # Constant in math module....
+
+print("\n****Constant Math Module*****\nPi Constant :-",m.pi)
+
+print("e :-",m.e)
+
+print("\nfloating point positive infinity inf :-",m.inf)
+
+print("\nFloating point not a Number \"NaN\" :-",m.nan)
